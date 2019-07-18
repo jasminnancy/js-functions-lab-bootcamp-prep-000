d6ef88f74dfcaeeba31aaff58636bfafb15705f8
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-18 17:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // write your code below!
 function happyHolidays(string) {return "Happy holidays!"}
-function happyHolidaysTo(string) {return
+function happyHolidaysTo(string) {return "Happy holidays, ${name}!"}
+function happyHolidayTo(string) {return "Happy ${holiday}, ${name}!"}
+function holidayCountdown(string) {return "It's ${days} days until ${holiday}!"}
